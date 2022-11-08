094cf61c704e57332dcc52f116e0d2836b66d807
David Almonacid <david.almonacidf@gmail.com>
2022-11-08 15:27:19 -0500

    Animation added

--- src/components/pages/home/Characters/CharacterCard.jsx
+++ src/components/pages/home/Characters/CharacterCard.jsx
@@ -1,13 +1,19 @@
+import './styles/CharacterCard.css';
+
 const CharacterCard = ({ name, status, gender, image }) => {
   return (
-    <article className='relative border-[12px] border-black rounded-xl w-[272px] h-[425px] overflow-hidden'>
-      <p className='absolute z-[1]'>{status}</p>
+    <article
+      className='relative border-[12px] border-black rounded-xl w-[272px] h-[425px] overflow-hidden'
+      id='character-card'
+    >
       <img
         src={image}
         alt={name}
-        className='absolute right-1/2 translate-x-2/4 h-full max-w-fit'
+        className='right-1/2 translate-x-2/4 h-full max-w-fit'
       />
-      <p className='absolute'>{gender}</p>
+      <p>{status}</p>
+      <p>{gender}</p>
+      <p className='bottom-0 w-full h-12 bg-slate-500 hidden'>{name}</p>
     </article>
   );
 };
