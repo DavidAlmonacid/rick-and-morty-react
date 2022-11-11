57093718745edaf3436d8bd9ae0822407d66d67a
DavidAlmonacid <65629741+DavidAlmonacid@users.noreply.github.com>
2022-11-11 17:50:45 +0000

    Adding routes with react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import AppHome from './components/pages/home/AppHome';
+import Character from './components/pages/character/Character';
+
+// Follow documentation
+// https://blog.webdevsimplified.com/2022-07/react-router/
 
 const App = () => {
-  return <AppHome />;
+  return (
+    <>
+      <AppHome />
+      <Character />
+    </>
+  );
 };
 
 export default App;
