c86a04e0fdd5303a7415f90aed3b78ba4ebea441
DavidAlmonacid <65629741+DavidAlmonacid@users.noreply.github.com>
2022-11-11 13:55:25 +0000

    Code formatter and icons added

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,17 @@
 module.exports = {
   content: ['./index.html', './src/**/*.jsx'],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Noto Sans', 'sans-serif'],
+    },
+    extend: {
+      dropShadow: {
+        light: [
+          '0 1px 8px rgba(255, 255, 255, 0.24)',
+          '0 0 1px rgba(255, 255, 255, 0.8)',
+        ],
+      },
+    },
   },
   plugins: [],
 };
