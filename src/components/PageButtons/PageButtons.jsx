d87db928a037c3068a3ba383b3a8d8e795bf6ec4
DavidAlmonacid <65629741+DavidAlmonacid@users.noreply.github.com>
2022-11-30 20:33:29 +0000

    Accessibility added

--- src/components/PageButtons/PageButtons.jsx
+++ src/components/PageButtons/PageButtons.jsx
@@ -11,6 +11,7 @@ const PageButtons = ({ page, setPage }) => {
     <div className='flex mx-12 py-2 justify-between'>
       <button
         disabled={page === 1 && true}
+        title={page !== 1 && `Previous: Page ${page - 1}`}
         className='disabled:opacity-60 disabled:cursor-not-allowed'
         onClick={handlePrev}
       >
@@ -19,6 +20,7 @@ const PageButtons = ({ page, setPage }) => {
 
       <button
         disabled={page === 42 && true}
+        title={page !== 42 && `Next: Page ${page + 1}`}
         className='disabled:opacity-60 disabled:cursor-not-allowed'
         onClick={handleNext}
       >
