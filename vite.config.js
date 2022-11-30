8f19bbdc6e8a04e83818638fc3be5c7ce36d0b96
DavidAlmonacid <65629741+DavidAlmonacid@users.noreply.github.com>
2022-11-30 23:31:37 +0000

    Removing GitHub pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,5 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: 'https://davidalmonacid.github.io/rick-and-morty-react/',
   plugins: [react()],
-  base: 'https://davidalmonacid.github.io/rick-and-morty-react/',
 });
