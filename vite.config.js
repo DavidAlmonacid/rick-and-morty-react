f4ee33663395e4c6b7f9bcf8a37de476620e78d4
DavidAlmonacid <65629741+DavidAlmonacid@users.noreply.github.com>
2022-11-30 21:48:50 +0000

    Fixed base URL

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/rick-and-morty-react/',
+  base: 'https://davidalmonacid.github.io/rick-and-morty-react/',
   plugins: [react()],
 });
